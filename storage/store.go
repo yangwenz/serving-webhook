ea97e39f5a3bae7356755299edf5736eec00af50
yangwenz <yangwenzhuo08@gmail.com>
2023-09-09 20:21:57 +0800

    First commit

--- storage/store.go
+++ storage/store.go
@@ -36,6 +36,7 @@ func NewS3Store(config utils.Config) (Store, error) {
 }
 
 // Need to enable S3 ACL and set the block public access ACL permissions
+// TODO: Use Gateway Endpoints for S3
 func (uploader *S3Store) Upload(fileReader io.Reader, fileKey string) (string, error) {
 	result, err := uploader.uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(uploader.config.AWSBucket),
